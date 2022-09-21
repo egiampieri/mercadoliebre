524f39a2f3ea80f9ae34bba035af80a88cf2d996
egiampieri <eve.giampieri@gmail.com>
2022-09-20 21:45:24 -0300

    probando

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.listen(Process.env.PORT || 4000, () => {
+    console.log("Servidor corriendo");
+});
+app.get('/',(req,res) => {
+    res.sendFile(path.resolve(__dirname, './src/views/home.html'));
+});
+app.get('/register',(req,res) => {
+    res.sendFile(path.resolve(__dirname, './src/views/register.html'));
+});
