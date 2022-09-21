c455688fb3b9ec4ca54e1398db03aa98688fcf5f
egiampieri <eve.giampieri@gmail.com>
2022-09-21 19:12:07 -0300

    login

--- app.js
+++ app.js
@@ -15,3 +15,6 @@ app.get('/',(req,res) => {
 app.get('/register',(req,res) => {
     res.sendFile(path.resolve(__dirname, './src/views/register.html'));
 });
+app.get('/login',(req,res) => {
+    res.sendFile(path.resolve(__dirname, './src/views/login.html'));
+});
